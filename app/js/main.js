43e1a4cddbeb9ceb7dc6cdf102f89be10a09c829
triplemonster221 <Boorder1993@yandex.ru>
2023-05-02 11:58:19 +0500

    added offer

--- app/js/main.js
+++ app/js/main.js
@@ -1,345 +1,263 @@
 document.addEventListener('DOMContentLoaded', function() {
+   
+
+  //поверка на выбранный элемент 
+
+  let calc = document.querySelector('.bf_calculation-contens');
+  let input = document.querySelectorAll('input[type="radio"]');
+  let checkoutwrapper = document.querySelector('.checkout-info-template');
+  let checkoutBtn = document.querySelector('.checkout-btn');
+  params = {
+    cabin: false,
+    glass: false,
+    color: false,
+    furnityr: false,
+    handles: false,
+    dostavka: false,
+    montazh: false,
+    height: false,
+    width: false
+  }
+
+  rezult = [
+
+  ]
+  
+  calc.addEventListener('click', checedElement);
+  checkoutBtn.addEventListener('click', checkout);
+
   
 
-//слайдер в оффере=====================
+  function checedElement(e){
+    //e.preventDefault()
 
-  // if (window.innerWidth <= 768) {
-  //   $('.offer-slider').slick({
-  //     //slidesToShow: 3,
-  //     //adaptiveHeight: true,
-  //     //dots: false,
-  //     //arrows: false,
-  //     //swipe: true,
-  //     settings: "unslick",
-  //     responsive: [
-  //         {
-  //           breakpoint: 650,
-  //           settings: {
-  //             slidesToShow: 1,
-  //             dots: true,
-  //             arrows: false,
-  //             settings: "unslick",
-  //           }
-  //         }
-  //       ]
-  //   })
+    // при выборе инпута, добавляем к заголовку класс 
+    function titleCheck(){
+      let parent = e.target.closest('.bf_calc-row');
+      let title = parent.querySelector('.bf_calc__title');
+      title.classList.add('title-check');
+      title.style.color = '#000000'
+    }
 
-  // }
+    // перебор инпутов
+
+          // выбор душевой кабины, и перекидка на 2 таб
+    if(e.target.dataset.cabin) {
+      let price = Number(e.target.dataset.price);
+      let titleCabune = e.target.nextElementSibling;
+      let paramsContent = document.querySelector('.params-conten .bf_calc-content');
+      let paramsInfo = document.querySelector('.params-conten-info');
+      params.cabin = titleCabune.innerHTML;
 
 
+      rezult.push(price);
 
-//значки цифр для слайдера слик ============================
-  // var helpers = {
-  //   addZeros: function (n) {
-  //       return (n < 10) ? '' + n : '' + n;
-  //   }
-  // };
 
-  // function sliderInit() {
-  //   var $slider = $('.recomendation-slider');
-  //   $slider.each(function() {
-  //     var $sliderParent = $(this).parent();
-  //     $(this).slick({
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //       dots: false,
-  //       infinite: true,
-  //       responsive: [
-  //         {
-  //           breakpoint: 767,
-  //           settings: {
-  //             adaptiveHeight: true
-  //           }
-  //         }
-  //       ]
-  //     });
-  
-  //     if ($(this).find('.recomendation-content').length > 1) {
-  //       $(this).siblings('.slides-numbers').show();
-  //     }
-  
-  //     $(this).on('afterChange', function(event, slick, currentSlide){
-  //       $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
-  //     });
-  
-  //     var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
-  //     $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
+      let contents = document.querySelectorAll('.bf_calculation-content');
+      let btn = document.querySelectorAll('.bf_calculation-tab');
 
-  //   });
-  // };
-    
-  // function sliderdirection() {
-  //     var $slider = $('.directions-slider');
-  //     $slider.each(function() {
-  //       var $sliderParent = $(this).parent();
-  //       $(this).slick({
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //         dots: false,
-  //         infinite: true,
-  //         //fade: true,
-  //         responsive: [
-  //           {
-  //             breakpoint: 767,
-  //             settings: {
-  //               //adaptiveHeight: true
-  //             }
-  //           }
-  //         ]
-  //       });
-    
-  //       if ($(this).find('.directions-content').length > 1) {
-  //         $(this).siblings('.slides-numbers').show();
-  //       }
-    
-  //       $(this).on('afterChange', function(event, slick, currentSlide){
-  //         $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
-  //       });
-    
-  //       var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
-  //       $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
-    
-  //     });
+      contents.forEach(el => {
+        el.classList.remove('bf_active');
+      })
+
+      btn.forEach(el => {
+        el.classList.remove('bf_active');
+      })
+
+      for(x = 0;x < contents.length;x++) {
+        contents[1].classList.add('bf_active')
+      }
+
+      for(x = 0;x < contents.length;x++) {
+        btn[1].classList.add('bf_active')
+      }
+      paramsInfo.style.display = 'none';
+      paramsContent.style.display = 'flex';
+    }
+
+
+
+    //выбор толщины стекла
+    if(e.target.dataset.glass) {
+      params.glass = e.target.nextElementSibling.innerHTML;
+      rezult.push(e.target.value);
+      titleCheck();
+
       
-  // };
+    }
+
+    if(e.target.dataset.color) {
+      params.color = e.target.nextElementSibling.innerHTML;
+      titleCheck();
+      rezult.push(e.target.value);
+    }
+
+    if(e.target.dataset.furnitur) {
+      params.furnityr = e.target.nextElementSibling.innerHTML;
+      rezult.push(e.target.value);
+      titleCheck();
+    }
+
+    if(e.target.dataset.handles) {
+      params.handles = e.target.nextElementSibling.style.backgroundImage;
+      //rezult.push(e.target.value);
+      titleCheck();
+    }
+
+    if(e.target.dataset.dostavka) {
+      params.dostavka = !params.dostavka;
       
-  // if (document.documentElement.clientWidth < 650) {
-  //   $('.recomendation-slider').slick({
-  //       slidesToShow: 1,
-  //       dots: true,
-  //       arrows: false,
-  //       settings: "unslick"
-  //   })
-  // } else {
-  //   sliderInit();
-  // }; 
+    }
 
-
-  // if (document.documentElement.clientWidth < 650) {
-  //   $('.directions-slider').slick({
-  //       slidesToShow: 1,
-  //       dots: true,
-  //       arrows: false                
-  //   })
-  // } else {
-  //   sliderdirection();
-  // };
-        
-
-// вывод модальных окон ============================
-    
-  // function bindModals(TriggerSelector, modalsSelector, closeSelector){
-  //     const trigger = document.querySelectorAll(TriggerSelector),
-  //         modals = document.querySelector(modalsSelector),
-  //         close = document.querySelector(closeSelector),
-  //         modalInner = document.querySelector('.modal'),
-  //         scroll = calcScroll();
-  //       //let text;
-
-  //     trigger.forEach(element => {
-  //         element.addEventListener('click', (e) => {
-  //             if(e.target){
-  //                 e.preventDefault();
-  //             }
-              
-  //             modalInner.classList.add('active');
-  //             modals.classList.add('active');
-  //             document.body.style.overflow = 'hidden';
-  //             document.body.style.marginRight = `${scroll}px`;
-              
-  //         }) 
-  //     });
-  //     if(close){  
-  //       close.addEventListener('click', () => {
-  //           modalInner.classList.remove('active');
-  //           modals.classList.remove('active');
-  //           document.body.style.overflow = '';
-  //           document.body.style.marginRight = `0px`;
-  //           if(modals.classList.contains("modal-video")){
-  //             let ModalVideo = modals.querySelector('.modal-content').innerHTML='';
-  //           }
-  //       })
-  //     }
-  //     if(modalInner){
-  //     modalInner.addEventListener('click', (e) => {
-  //         if(e.target === modalInner) {
-  //             if(modals) {
-  //               modals.classList.remove('active');
-  //             }
-  //             modalInner.classList.remove('active');
-              
-  //             document.body.style.overflow = '';
-  //             document.body.style.marginRight = `0px`;
-              
-  //         }
-  //     }) 
-  //   }  
-
-  //     // убираем скролл
-
-  // function calcScroll() {
-  //   let div = document.createElement('div');
-  
-  //   div.style.width = '50px';
-  //   div.style.height = '50px';
-  //   div.style.overflowY = 'scroll';
-  //   div.style.visibility = 'hidden';
-  
-  //   document.body.appendChild(div);
-  //   let scrollWidth = div.offsetWidth - div.clientWidth;
-  //   div.remove();
-  
-  //   return scrollWidth;
-  // }
+    if(e.target.dataset.montazh) {
+      params.montazh = !params.montazh;
       
+    }
 
-  //     // modalExit();
-  //     function modalExit() {
+    if(e.target.dataset.height) {
+      let element =  e.target.nextElementSibling;
+      let min = Number(element.querySelector('.min').dataset.min);
+      let max = Number(element.querySelector('.max').dataset.max);
+      
+      e.target.addEventListener('blur', function(){
+          if(e.target.value < min) {
+            e.target.value = min
+          }
+
+          if(e.target.value > max) {
+            e.target.value = max
+          }
+
+          params.height = e.target.value
           
-  //         let popupExit = document.querySelector('.modal-away');
-  //         let close = document.querySelector('.modal-away .clouse');
-  //         let count = 0
-  //         if(popupExit){
-  //         $(document).mouseleave(function (e) {
-  //             if (count == 1) {
-  //                 return
-                  
-  //             }
-  //             if (e.clientY < 1 && !modalInner.classList.contains('active') ){
-                  
-  //                   modalInner.classList.add('active');
-  //                   popupExit.classList.add('active');
-  //                   document.body.style.overflow = 'hidden';
-  //                   document.body.style.marginRight = `${scroll}px`;
-            
-                
-  //                 if (popupExit.classList.contains('active')) {
-  //                       document.body.addEventListener('keydown', function (e) {
-  //                         if (e.keyCode === 27) {
-  //                             modalInner.classList.remove('active');
-  //                             popupExit.classList.remove('active');
-  //                             document.body.style.overflow = '';
-  //                         }
-  //                     })
-  //                     modalInner.addEventListener('click', (e) => {
-  //                       if(e.target === modalInner) {
-  //                           modalInner.classList.remove('active');
-  //                           popupExit.classList.remove('active');
-  //                           document.body.style.overflow = '';
-  //                           document.body.style.marginRight = `0px`;
-  //                       }
-  //                     }) 
+      })
 
-  //                     if(close){
-  //                         close.addEventListener('click', () => {
-  //                           console.log(11);
-  //                           modalInner.classList.remove('active');
-  //                           popupExit.classList.remove('active');
-  //                           document.body.style.overflow = '';
-  //                           document.body.style.marginRight = `0px`;
-  //                       })
-  //                     }
-  //                 }
-                  
-  //             }
-  //             count++;
-  //         })
-  //       }
-  //     }
+      params.height = e.target.value
+      titleCheck();
+    }
 
-  //     setTimeout(function() { 
-  //       modalExit();
-  //     }, 100);
+    if(e.target.dataset.width) {
+      let element =  e.target.nextElementSibling;
+      let min = Number(element.querySelector('.min').dataset.min);
+      let max = Number(element.querySelector('.max').dataset.max);
       
-  // }; 
+      e.target.addEventListener('blur', function(){
+          if(e.target.value < min) {
+            e.target.value = min
+          }
 
-  //bindModals('.header__calBack', '.modal-callback', '.modal-callback  .clouse');
-         
+          if(e.target.value > max) {
+            e.target.value = max
+          }
 
-// валидация ============================
+          params.width = e.target.value
+      })
 
-  // const numbers = document.querySelectorAll('.phone');
+      params.width = e.target.value
+      titleCheck();
+    }
 
-  // numbers.forEach(element => {
+    console.log(params)
 
-  //     // var phoneMask = IMask(element, {
-  //     //   mask: '+{7}(000)000-00-00',
-  //     //   lazy: false,  // make placeholder always visible
-  //     //   placeholderChar: '_'     // defaults to '_'
-  //     // });
+  }
 
-  //     $(element).inputmask({
-  //       "mask": "+ 9 (999) 999-9999",
-  //       showMaskOnHover: false,
-  //       "oncomplete": function(){
-  //         var value = $(this).val();
-  //         $(this).val(value.replace(/(\+)(\s|)(8)/g,"$1$1"+7))
-  //       }
-  //   });
+  //нажатие на кнопку расчитать 
 
-  // })
+  function checkout(){
+    let contents = document.querySelectorAll('.bf_calculation-content');
+    let btn = document.querySelectorAll('.bf_calculation-tab');
+    let checkoutMessage = document.querySelector('.checkout-info-message');
 
-  // jQuery.validator.addMethod("checkMask", function(value, element) {
-  //   return /\+ \d{1} \(\d{3}\) \d{3}-\d{4}/g.test(value); 
-  // });
+    //проверка на заполнение обьекта данными
+    if(params.glass && params.color &&  params.furnityr &&  params.handles &&  params.height && params.width) {
 
-  // $(".sectionForm").each(function() {
-  //     $(this).validate({
-  //       rules: {
-  //         name:{
-  //           required: true,
-  //         },
-  //         phone: {
-  //           required: true,
-  //           checkMask: true
-  //         },
-  //         email: {
-  //           required: true,
-  //           email: true
-  //         },
-  //         town: {
-  //           required: true,
-  //         }
-  //       }
-  //     });
-  // });
-
-
-//---------------ANIMATION============================
-  // const animItems = document.querySelectorAll('._anim-items');
-
-  // if (animItems.length > 0){
-  //     window.addEventListener('scroll', animOnScroll);
-  //     function animOnScroll(params){
-  //         for(let index = 0; index < animItems.length; index++){
-  //             const animItem = animItems[index];
-  //             const animItemHeight = animItem.offsetHeight;
-  //             const animItemOffset = offset(animItem).top;
-  //             const animStart = 4;
-
-  //             let animItemPoint = window.innerHeight - animItemHeight / animStart;
-  //             if(animItemHeight > window.innerHeight){
-  //                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
-  //             }
-
-  //             if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
-  //                 animItem.classList.add('_active');
-  //             }else{
-  //                 if(!animItem.classList.contains('_no-anim')){
-  //                     animItem.classList.remove('_active');
-  //                 }
-  //             }
-  //         }
-  //     }
-  //     animOnScroll();
-  // }
-  // function offset(el){
-  //     const rect = el.getBoundingClientRect(),
-  //         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  //         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  //     return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-  // }
+      contents.forEach(el => {
+        el.classList.remove('bf_active');
+      })
   
+      btn.forEach(el => {
+        el.classList.remove('bf_active');
+      })
+  
+      for(x = 0;x < contents.length;x++) {
+        contents[2].classList.add('bf_active')
+      }
+  
+      for(x = 0;x < contents.length;x++) {
+        btn[2].classList.add('bf_active')
+      }
+      checkoutMessage.remove();
+      addTemplate();
+      
+
+    } else { //если каких то данных нет то маркировать заголовок
+      let title = document.querySelectorAll('.bf_calc__title ');
+
+      title.forEach(el => {
+        console.log(el);
+        if(!el.classList.contains('title-check')) {
+          el.style.color="red";
+        }else {
+          
+        }
+      })
+    }
+   
+    
+  }
+
+
+  function addTemplate(){
+
+    let template = 
+    `
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Душевая кабина</div>
+      <div class="checkout-row-right">${params.cabin}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Толщина стекла:</div>
+      <div class="checkout-row-right">${params.glass}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Цвет стекла:</div>
+      <div class="checkout-row-right">${params.color}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Цвет фурнитуры:</div>
+      <div class="checkout-row-right">${params.furnityr}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Тип ручки:</div>
+      <div class="checkout-row-right"> <img src="img/knob1.jpg" alt=""/></div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Доставка:</div>
+      <div class="checkout-row-right">${params.dostavka}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Монтаж:</div>
+      <div class="checkout-row-right">${params.montazh}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Высота (H):</div>
+      <div class="checkout-row-right">${params.height}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Длина (A):</div>
+      <div class="checkout-row-right">${params.width}</div>
+    </div>
+    <div class="checkout-row"> 
+      <div class="checkout-row-left">Итого</div>
+      <div class="checkout-row-right">12 000 руб.</div>
+    </div>
+    `;
+
+
+    checkoutwrapper.innerHTML = template;
+  }
+ 
+
 //---------------STOP ANIMATION============================
 
 function burger(){
@@ -423,32 +341,5 @@ burger();
 // ============================STOP DATE============================
 
 
-//--------SHOW-MOREE============================
-  // function showMore(){
-  //   let btns = document.querySelectorAll('.reviews-box__moree');
 
-  //   btns.forEach(el => {
-  //     el.addEventListener('click', function(){
-  //       let wrapper = this.closest('.reviews-box');
-  //       wrapper.classList.toggle('_active');
-  //     })
-  //   })
-  // }
-  // showMore()
-
-//--------STOP SHOW-MOREE============================
-
-//--------akkardion============================
-  // function accardion(){
-  //   let accardionTop = document.querySelectorAll('.accardion-top');
-    
-  //   accardionTop.forEach(el => {
-  //       el.addEventListener('click', function(){
-  //          this.closest('.accardion-box').classList.toggle('_active'); 
-  //       })
-  //   })
-  // }
-
-  // accardion();
-//--------STOP akkardion============================
 })
